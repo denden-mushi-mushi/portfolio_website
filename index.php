a858a527e4b36de0782ab1ce476584d4bb597eee
LAPTOP-SHPJCGII\kawachi kazuki <kazukiwammu@gmail.com>
2022-02-26 00:29:04 +0900

    header

--- index.php
+++ index.php
@@ -2,11 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <title>Kazuki Portfolio</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style/style.css">
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=swap" rel="stylesheet">
 </head>
 <body>
+    <!-- header section start -->
+    <header>
+        <div id="user">
+            <img src="images/winter/winter_mountain4.jpg" alt="my_icon">
+            <h1 id="name">Kazuki</h1>
+            <p>front end programmer</p>
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="#home_wrapper">Home</a></li>
+                <li><a href="#about_wrapper">About</a></li>
+                <li><a href="#skills_wrapper">Skills</a></li>
+                <li><a href="#portfolio_wrapper">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <!-- header section finish -->
     
 </body>
 </html>
